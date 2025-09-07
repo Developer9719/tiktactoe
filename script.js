12edb764c865f55bf60a99ae2d355cae87264567
Developer9719 <justin.bat19@gmail.com>
2025-09-06 20:06:13 -0400

    Pseudo code written up

---
+++ script.js
@@ -0,0 +1,15 @@
+/**
+ * Pseudo code for a simple Tic Tac Toe game
+ * 
+ * Goal: Have as little global code as possible.
+ * - Use factory functions
+ * - Put the factories in IIFEs if there only needed once
+ * - Everything needs to be inside one of the objects
+ * - Focus on playing the game in the console before adding the UI
+ *   - Create a new object to handle the UI
+ *   - Add user input for player names, start and restart buttons and a results screen
+ * 
+ * 1. Create a 3x3 grid for the game board in an array inside an object.
+ * 2. Store players as objects with properties like name and symbol (X or O).
+ * 3. Store the game flow as an object with properties like currentPlayer and gameStatus.
+ */
