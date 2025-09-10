2e8bbc82e40c9b821c10e8f1cfa8edf4dc0f468a
Developer9719 <justin.bat19@gmail.com>
2025-09-09 22:02:37 -0400

    Console game code started

--- script.js
+++ script.js
@@ -14,21 +14,69 @@
  * 3. Store the game flow as an object with properties like currentPlayer and gameStatus.
  */
 
-(function() { // IIFE for game board module
-    const board = [ // Game board array
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', '']
-    ];
-})();
+// Step 1: The Working Console Game
+const gameBoard = [ // 3x3 grid
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+];
 
-function createPlayer(name, symbol) { // Player factory function
-    return {
-        name: name,
-        symbol: symbol
-    };
+const player1 = {
+    name: 'Player 1',
+    symbol: 'X'
+};
+
+const player2 = {
+    name: 'Player 2',
+    symbol: 'O'
+};
+
+function gameFlow(gameStatus = 'start', playerTurn) {
+    if (gameStatus === 'start') {
+        console.log('Game started');
+        console.log('Player 1 is X');
+        console.log('Player 2 is O');
+        console.log('Player 1 starts');
+        currentPlayer = player1;
+        gameStatus = 'inProgress';
+        // Wait for player input
+        
+
+        gameFlow(gameStatus, currentPlayer); //
+    } else if (gameStatus === 'inProgress') {
+        console.log(`It's ${currentPlayer.name}'s turn.`);
+        if (playerTurn = player1) {
+            // Play turn
+
+            if (checkWin(playedSpaces())) { // Return true if win
+                gameStatus = 'win';
+            } else {
+                playerTurn = player2;
+                gameFlow(gameStatus, playerTurn);
+            }
+        } else if (playerTurn = player2) {
+
+        }
+    } else if (gameStatus === 'win') {
+        console.log(`${currentPlayer.name} wins!`);
+    } else if (gameStatus === 'draw') {
+        console.log('The game is a draw!');
+    }
 }
 
-// Create the player objects
-const player1 = createPlayer('Player 1', 'X');
-const player2 = createPlayer('Player 2', 'O');
+function checkWin(board) {
+
+}
+
+function playedSpaces() {
+    
+}
+
+gameFlow();
+
+// Step 2: The User Interface
+
+
+// Step 3: Refactoring for using Factories and IIFEs and code cleanup
+
+
